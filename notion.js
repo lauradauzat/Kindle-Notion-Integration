8765b37fbfd0cb8aa1e00bce04473625726b9562
Laura Dauzat <63320636+lauradauzat@users.noreply.github.com>
2023-01-02 11:52:15 +0100

    basic create function using json file

--- notion.js
+++ notion.js
@@ -9,3 +9,63 @@ async function getDatabase() {
 
 getDatabase()
 
+const myClippings = require('./KindleHighlights.json')
+
+console.log(myClippings); 
+
+
+function createHighlights(highlights) {
+    notion.pages.create({
+        parent: {
+            database_id: process.env.NOTION_DATABASE_ID
+        }, 
+        properties: {
+            [process.env.NOTION_TITLE_ID]: {
+                title: [
+                  {
+                    type: "text",
+                    text: {
+                      content: highlights.title,
+                    },
+                  },
+                ],
+              },
+        },
+
+        children: [
+            {
+                object: "block",
+                heading_2: {
+                    rich_text: [
+                        {
+                            text: {
+                                content: highlights.heading
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                object: "block",
+                paragraph: {
+                    rich_text: [
+                        {
+                            text: {
+                                content: highlights.content,
+                            },
+                            
+                        }
+                    ],
+                    color: "default"
+                }
+            }
+        ]
+
+    })
+}
+
+createHighlights({
+    title: myClippings[0].BookTitle, 
+    heading: 'Hightlights',
+    content: myClippings[0].Content,
+});
