387f9d28935aa9a6c2e9e7451fa87bbf6c460559
Laura Dauzat <63320636+lauradauzat@users.noreply.github.com>
2023-01-02 12:54:11 +0100

    module to parse myclippings that works

--- notion.js
+++ notion.js
@@ -4,14 +4,14 @@ const notion= new Client({auth: process.env.NOTION_API_KEY})
 
 async function getDatabase() {
     const response = await notion.databases.retrieve({database_id: process.env.NOTION_DATABASE_ID})
-    console.log(response)
+   // console.log(response)
 }
 
 getDatabase()
 
 const myClippings = require('./KindleHighlights.json')
 
-console.log(myClippings); 
+//console.log(myClippings); 
 
 
 function createHighlights(highlights) {
@@ -69,3 +69,17 @@ createHighlights({
     heading: 'Hightlights',
     content: myClippings[0].Content,
 });
+
+const kindleClippings = require('@darylserrano/kindle-clippings');
+
+const exampleEntry = `test entry(Jean Michel)
+- La subrayado en la página 6 | posición 36-40 | Añadido el lunes, 30 de septiembre de 2019 18:00:39
+
+Contenteoiueoiueoiueoiuoeiuoeiuoeiuoiu
+==========`;
+
+let entries = kindleClippings.readKindleClipping(exampleEntry);
+let parsedEntries = kindleClippings.parseKindleEntries(entries);
+console.log(JSON.stringify(parsedEntries[0].toJSON()));
+var entriesParsed =
+  kindleClippings.organizeKindleEntriesByBookTitle(parsedEntries);
--- server.js
+++ server.js
@@ -9,3 +9,5 @@ app.get('/', (req,res) => {
 })
 
 app.listen(process.env.PORT)
+
+
