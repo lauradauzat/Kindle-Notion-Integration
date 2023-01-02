b9d7155471ea9a5a1e03a9880aadc9e7739cedf9
Laura Dauzat <63320636+lauradauzat@users.noreply.github.com>
2023-01-02 18:45:58 +0100

    create ok update infos ready but not working yet

--- notion.js
+++ notion.js
@@ -1,17 +1,39 @@
 const { Client } = require("@notionhq/client")
 var fs = require("fs");
-
-
 const notion= new Client({auth: process.env.NOTION_API_KEY})
 
+const kindleClippings = require('@darylserrano/kindle-clippings');
+var clippingsText = fs.readFileSync("./My Clippings.txt").toString('utf-8');
+
+let currentBooks = [];
+
+
 async function getDatabase() {
-    const response = await notion.databases.retrieve({database_id: process.env.NOTION_DATABASE_ID})
-   // console.log(response)
-}
+
+    const response = await notion.databases.query({database_id: process.env.NOTION_DATABASE_ID})
+    //console.log(response.results)
+    var resultsRetrieved = response.results; 
+    resultsRetrieved.forEach( result => {
+       // console.log(result.properties.Title.title[0].plain_text, result.id);
+        let thisTitle = result.properties.Title.title[0].plain_text; 
+        let thisId = result.id; 
+        //console.log(thisTitle, thisId)
+        var obj = {
+            id: thisId,
+            title: thisTitle
+        }
+        currentBooks.push(obj);
+       
+      
+ 
+
+    })
+
+    //console.log(currentBooks);
+    return currentBooks;
+} 
 
 getDatabase()
-
-
 
 
 function createHighlights(highlights) {
@@ -74,46 +96,120 @@ function createHighlights(highlights) {
 }
 
 
-
-const kindleClippings = require('@darylserrano/kindle-clippings');
-
-
-var clippingsText = fs.readFileSync("./My Clippings.txt").toString('utf-8');
+ async function updateHighLights(highlights, bookId) {
+    const response =  await notion.pages.update({
+        page_id: bookId,
+        children: [
+            {
+                object: "block",
+                heading_2: {
+                    rich_text: [
+                        {
+                            text: {
+                                content: highlights.heading
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                object: "block",
+                paragraph: {
+                    rich_text: [
+                        {
+                            text: {
+                                content: highlights.content,
+                            },
+                            
+                        }
+                    ],
+                    color: "default"
+                }
+            }
+        ]
+      });
+      console.log(response);
+}
 
 //console.log(clippingsText)
 
 let entries = kindleClippings.readKindleClipping(clippingsText);
-
 let parsedEntries = kindleClippings.parseKindleEntries(entries);
 
 
 //console.log(JSON.stringify(parsedEntries[0].toJSON()));
 
 
+
 var entriesParsed = kindleClippings.organizeKindleEntriesByBookTitle(parsedEntries);
 
-entriesParsed.forEach(books => {
-    var thisBookHighlights = '';
-    console.log('-------------');
-    console.log(books[0].bookTile);
-    console.log(books[0].authors);
-    books.forEach(hl => {
-        console.log('+++++++++++');
-        console.log(hl.content)
-        thisBookHighlights +=
-        `
-        ${hl.dateOfCreation}
-        ${hl.location}
-        ${hl.content}
-        `
-    })
+
+
+
+ const HandleOldAndNewBooks = async () => {
+    const updatedCurrentBooks = await getDatabase()
+    // do something else here after firstFunction completes
+    //console.log(updatedCurrentBooks);
+
+    entriesParsed.forEach(books => {
+        var thisBookHighlights = '';
+        
+        // console.log('-------------');
+       // console.log(books[0].bookTile);
+        var epBookTitle = books[0].bookTile;
+        console.log('----------');
+        console.log(epBookTitle);
+       // console.log('xxxxxxx');
+        //console.log(updatedCurrentBooks[0].title);
+
+       //console.log(currentBooks);
     
-     createHighlights({
-        title: books[0].bookTile, 
-        author: books[0].authors,
-        heading: 'Hightlights',
-        content: thisBookHighlights,
-    });
- });
+    
+        // console.log(books[0].authors);
+        books.forEach(hl => {
+            // console.log('+++++++++++');
+            // console.log(hl.content)
+            thisBookHighlights +=
+            `Location : ${hl.location} | Date :  ${hl.dateOfCreation}
+            ${hl.content}
+            `
+        })
+        
+        //  createHighlights({
+        //     title: books[0].bookTile, 
+        //     author: books[0].authors,
+        //     heading: 'Highlights',
+        //     content: thisBookHighlights,
+        // });
+  
+        if(updatedCurrentBooks.map(e => e.title).indexOf(epBookTitle)!==-1) {
+            console.log('Livre déjà enregistré');
+            console.log(updatedCurrentBooks.map(e => e.title).indexOf(epBookTitle));
+            var index = updatedCurrentBooks.map(e => e.title).indexOf(epBookTitle);
+               //recupérer la ref sur Notion 
+            var bookId = updatedCurrentBooks[index].id;
+            //update
+            updateHighLights({
+                heading: 'new Highlights',
+                content: thisBookHighlights,
+            }, bookId)
+         
+        
+            //update son content
+           } else {
+            console.log('Nouveau Livre');
+            createHighlights({
+                title: books[0].bookTile, 
+                author: books[0].authors,
+                heading: 'Highlights',
+                content: thisBookHighlights,
+             });
+            
+           }
+     });
+    
 
 
+  }
+
+  HandleOldAndNewBooks();
