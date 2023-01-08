aefa8b14b45efcb87da168c1f98eb5c582b43422
Laura Dauzat <63320636+lauradauzat@users.noreply.github.com>
2023-01-08 13:15:04 +0100

    updates fully working !"

--- notion.js
+++ notion.js
@@ -3,6 +3,7 @@ var fs = require("fs");
 const notion= new Client({auth: process.env.NOTION_API_KEY})
 
 const kindleClippings = require('@darylserrano/kindle-clippings');
+const { isGeneratorFunction } = require("util/types");
 var clippingsText = fs.readFileSync("./My Clippings.txt").toString('utf-8');
 
 let currentBooks = [];
@@ -37,6 +38,23 @@ getDatabase()
 
 
 function createHighlights(highlights) {
+    const highlightsContent = highlights.content 
+    const quoteBlocks = highlightsContent.map((highlight) => ({
+        object: "block",
+        type: "quote",
+        quote: {
+          rich_text: [
+            {
+              type: "text",
+              text: {
+                content: highlight,
+              },
+            },
+          ],
+        },
+      }));
+
+
     notion.pages.create({
         parent: {
             database_id: process.env.NOTION_DATABASE_ID
@@ -63,72 +81,53 @@ function createHighlights(highlights) {
             },  
         },
 
-        children: [
-            {
-                object: "block",
-                heading_2: {
-                    rich_text: [
-                        {
-                            text: {
-                                content: highlights.heading
-                            }
-                        }
-                    ]
-                }
-            },
-            {
-                object: "block",
-                paragraph: {
-                    rich_text: [
-                        {
-                            text: {
-                                content: highlights.content,
-                            },
-                            
-                        }
-                    ],
-                    color: "default"
-                }
-            }
-        ]
+        children: quoteBlocks,
 
     })
 }
 
-
  async function updateHighLights(highlights, bookId) {
-    const response =  await notion.pages.update({
-        page_id: bookId,
-        children: [
-            {
-                object: "block",
-                heading_2: {
+    //console.log(highlights)
+
+   const highlightsContent = highlights.content 
+
+   //ignore highlight that already exist 
+
+
+   for (const highlight of highlightsContent) {
+    const result = await quoteAlreadyExist(bookId, highlight);
+
+     console.log(result);
+      if (result === undefined) {
+           console.log('appending new highlight ! ');
+            const response = await notion.blocks.children.append({
+                block_id: bookId,
+                children: [
+                {
+                    object: 'block',
+                    type: 'quote',
+                    quote: {
                     rich_text: [
                         {
-                            text: {
-                                content: highlights.heading
-                            }
-                        }
-                    ]
-                }
-            },
-            {
-                object: "block",
-                paragraph: {
-                    rich_text: [
-                        {
-                            text: {
-                                content: highlights.content,
-                            },
-                            
-                        }
+                        type: 'text',
+                        text: {
+                            content: highlight,
+                        },
+                        },
                     ],
-                    color: "default"
-                }
-            }
-        ]
-      });
-      console.log(response);
+                    },
+                },
+                ],
+            });
+            //console.log(response);
+        } else {
+            console.log('All highlight already on Notion ! ');
+        }
+      
+   }
+   
+      
+    
 }
 
 //console.log(clippingsText)
@@ -136,15 +135,11 @@ function createHighlights(highlights) {
 let entries = kindleClippings.readKindleClipping(clippingsText);
 let parsedEntries = kindleClippings.parseKindleEntries(entries);
 
-
 //console.log(JSON.stringify(parsedEntries[0].toJSON()));
-
-
 
 var entriesParsed = kindleClippings.organizeKindleEntriesByBookTitle(parsedEntries);
 
-
-
+//console.log(entriesParsed);
 
  const HandleOldAndNewBooks = async () => {
     const updatedCurrentBooks = await getDatabase()
@@ -152,13 +147,13 @@ var entriesParsed = kindleClippings.organizeKindleEntriesByBookTitle(parsedEntri
     //console.log(updatedCurrentBooks);
 
     entriesParsed.forEach(books => {
-        var thisBookHighlights = '';
+        var thisBookHighlights = [];
         
         // console.log('-------------');
        // console.log(books[0].bookTile);
         var epBookTitle = books[0].bookTile;
-        console.log('----------');
-        console.log(epBookTitle);
+        //console.log('----------');
+        //console.log(epBookTitle);
        // console.log('xxxxxxx');
         //console.log(updatedCurrentBooks[0].title);
 
@@ -169,11 +164,14 @@ var entriesParsed = kindleClippings.organizeKindleEntriesByBookTitle(parsedEntri
         books.forEach(hl => {
             // console.log('+++++++++++');
             // console.log(hl.content)
-            thisBookHighlights +=
+            var blockHl = 
             `Location : ${hl.location} | Date :  ${hl.dateOfCreation}
             ${hl.content}
-            `
+            `; 
+            thisBookHighlights.push(blockHl); 
+
         })
+        //console.log(thisBookHighlights);
         
         //  createHighlights({
         //     title: books[0].bookTile, 
@@ -183,25 +181,27 @@ var entriesParsed = kindleClippings.organizeKindleEntriesByBookTitle(parsedEntri
         // });
   
         if(updatedCurrentBooks.map(e => e.title).indexOf(epBookTitle)!==-1) {
-            console.log('Livre déjà enregistré');
-            console.log(updatedCurrentBooks.map(e => e.title).indexOf(epBookTitle));
+            //console.log('Livre déjà enregistré');
+            //console.log(updatedCurrentBooks.map(e => e.title).indexOf(epBookTitle));
             var index = updatedCurrentBooks.map(e => e.title).indexOf(epBookTitle);
                //recupérer la ref sur Notion 
             var bookId = updatedCurrentBooks[index].id;
+            
+            //test retrieve 
+
+        
             //update
             updateHighLights({
-                heading: 'new Highlights',
                 content: thisBookHighlights,
             }, bookId)
          
         
             //update son content
            } else {
-            console.log('Nouveau Livre');
+            //console.log('Nouveau Livre');
             createHighlights({
                 title: books[0].bookTile, 
                 author: books[0].authors,
-                heading: 'Highlights',
                 content: thisBookHighlights,
              });
             
@@ -209,7 +209,50 @@ var entriesParsed = kindleClippings.organizeKindleEntriesByBookTitle(parsedEntri
      });
     
 
-
+ 
   }
 
   HandleOldAndNewBooks();
+
+
+  async  function quoteAlreadyExist(bookId, quoteContent) {
+    // Retrieve a list of all the blocks in the page
+
+    const response =   await notion.blocks.children.list({
+        block_id: bookId,
+      });
+     
+    const result = response.results;
+
+    for (const thisResult of result) { 
+
+      if(thisResult.quote) {
+            const blocks = thisResult.quote.rich_text[0].text.content; 
+            //console.log(blocks);
+            
+            if (blocks === quoteContent) {
+              //console.log('entry already found : '+ blocks + quoteContent);
+              return true; 
+            } 
+
+          } 
+    }
+
+   
+
+
+ 
+    // // Search for a quote block with the desired content
+    // const quoteBlock = response.children.find((block) => {
+    //   return block.type === "quote" && block.quote.rich_text[0].text.content === quoteContent;
+    // });
+  
+    // // Return true if the quote block was found, false otherwise
+    // return quoteBlock === undefined;
+
+ }
+
+  
+  
+  
+  
